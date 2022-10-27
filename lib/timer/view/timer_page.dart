eac9df79228f05263c4591680ab005f1e3b8c933
Cyrille Dakhlia <dakhliacyrille@gmail.com>
2022-10-27 10:33:37 +0200

    refacto: move StackAnimation widget in a new file

--- lib/timer/view/timer_page.dart
+++ lib/timer/view/timer_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_timer/components/stack_animation.dart';
 import 'package:flutter_timer/ticker.dart';
 import 'package:flutter_timer/timer/bloc/timer_bloc.dart';
 
@@ -189,111 +190,5 @@ class ActionButtons extends StatelessWidget {
         ),
       ]
     ];
-  }
-}
-
-class StackOpenAnimation extends StatelessWidget {
-  Duration duration;
-  double separationDistance;
-  Widget leftWidget, rightWidget;
-  bool leftWidgetIsOver;
-
-  StackOpenAnimation(
-      {Key? key,
-      required this.duration,
-      required this.separationDistance,
-      required this.leftWidget,
-      required this.rightWidget,
-      this.leftWidgetIsOver = true})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StackAnimation(
-      duration: duration,
-      begin: 0.0,
-      end: separationDistance,
-      leftWidget: leftWidget,
-      rightWidget: rightWidget,
-      leftWidgetIsOver: leftWidgetIsOver,
-    );
-  }
-}
-
-class StackCloseAnimation extends StatelessWidget {
-  Duration duration;
-  double separationDistance;
-  Widget leftWidget, rightWidget;
-  bool leftWidgetIsOver;
-
-  StackCloseAnimation(
-      {Key? key,
-      required this.duration,
-      required this.separationDistance,
-      required this.leftWidget,
-      required this.rightWidget,
-      this.leftWidgetIsOver = true})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StackAnimation(
-      duration: duration,
-      begin: separationDistance,
-      end: 0.0,
-      leftWidget: leftWidget,
-      rightWidget: rightWidget,
-      leftWidgetIsOver: leftWidgetIsOver,
-    );
-  }
-}
-
-class StackAnimation extends StatelessWidget {
-  Duration duration;
-  double begin, end;
-  Widget leftWidget, rightWidget;
-  bool leftWidgetIsOver;
-  StackAnimation(
-      {Key? key,
-      required this.duration,
-      required this.begin,
-      required this.end,
-      required this.leftWidget,
-      required this.rightWidget,
-      this.leftWidgetIsOver = true})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TweenAnimationBuilder<double>(
-      duration: duration,
-      tween: Tween<double>(begin: begin, end: end),
-      curve: Curves.easeInOut,
-      builder: (_, value, __) => Stack(
-        clipBehavior: Clip.none,
-        alignment: Alignment.center,
-        children: [
-          if (leftWidgetIsOver) ...[
-            Padding(
-              padding: EdgeInsets.only(left: value),
-              child: rightWidget,
-            ),
-            Padding(
-              padding: EdgeInsets.only(right: value),
-              child: leftWidget,
-            ),
-          ] else ...[
-            Padding(
-              padding: EdgeInsets.only(right: value),
-              child: leftWidget,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: value),
-              child: rightWidget,
-            ),
-          ]
-        ],
-      ),
-    );
   }
 }
