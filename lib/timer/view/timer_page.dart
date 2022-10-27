4317518bbcf102230f5ec1f90ad164d5e125cd18
Cyrille Dakhlia <dakhliacyrille@gmail.com>
2022-10-27 18:04:01 +0200

    refacto: change timer text style

--- lib/timer/view/timer_page.dart
+++ lib/timer/view/timer_page.dart
@@ -83,7 +83,10 @@ class TimerText extends StatelessWidget {
     return Center(
       child: Text(
         '$minutesString:$secondsString',
-        style: Theme.of(context).textTheme.headline1,
+        style: Theme.of(context).textTheme.headline1?.copyWith(
+              color: Colors.white,
+              fontWeight: FontWeight.w200,
+            ),
       ),
     );
   }
